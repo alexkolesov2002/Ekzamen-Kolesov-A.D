47c6b59b69a5b485da93eb11abb03a617e357225
Алексей Колесов <alladin.kolesov@mail.ru>
2021-07-05 09:41:36 +0300

    Добавьте файлы проекта.

---
+++ Ekzamen Kolesov A.D/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ekzamen_Kolesov_A.D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
